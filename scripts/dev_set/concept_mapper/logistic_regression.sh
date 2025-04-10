80f23d8bce972a9c0060eec56caeeca849166a47
vedant29 <vedantpungliya29@gmail.com>
2025-04-10 18:15:06 -0500

    Files

--- scripts/dev_set/concept_mapper/logistic_regression.sh
+++ scripts/dev_set/concept_mapper/logistic_regression.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-
 scriptDir=src/concept_mapper
-fileDir=position_representation_info  #saliency_representation_info #
-classifierDir=../../train_set/classifier_mapping/result/model
+fileDir=eraser_movie_dev/layer12/explanation  # Changed to match your explanation directory
+classifierDir=eraser_movie/layer12/model/model  # Changed to match where your classifier is saved
 
-mkdir "latent_concepts"
-mkdir "latent_concepts/position_prediction"
-
+# Create output directories
+mkdir -p eraser_movie_dev/layer12/explanation/latent_concepts/position_prediction
 
 layer=12
-echo ${layer}
+echo "Processing layer ${layer}"
 python ${scriptDir}/logistic_regression.py \
-  --test_file_path ${fileDir}/explanation_words_representation_layer${layer}.csv \
-  --layer ${layer} \
-  --save_path ./latent_concepts/position_prediction/ \
-  --do_predict \
-  --classifier_file_path $classifierDir/layer_${layer}_classifier.pkl \
-  --load_classifier_from_local
+    --test_file_path ${fileDir}/cluster_Labels_90%/clusterLabel_layer${layer}.json \
+    --layer ${layer} \
+    --save_path ./eraser_movie_dev/layer12/explanation/latent_concepts/position_prediction/ \
+    --do_predict \
+    --classifier_file_path ${classifierDir}/layer_${layer}_classifier.pkl \
+    --load_classifier_from_local
--- scripts/dev_set/concept_mapper/match_representation.sh
+++ scripts/dev_set/concept_mapper/match_representation.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-scriptDir=../../../src/concept_mapper
-input=movie_dev_subset.txt
-working_file=$input.tok.sent_len
+scriptDir=src/concept_mapper
+input=eraser_movie_dev/layer12/movie_dev_subset.txt
+working_file=${input}.tok.sent_len
 
-dataPath=../clustering/eraser_movie_dev
+dataPath=eraser_movie_dev
 minfreq=0
-maxfreq=1000000
-delfreq=1000000
+maxfreq=15000
+delfreq=15000
 
-savePath=position_representation_info
-mkdir $savePath
+savePath=eraser_movie_dev/layer12/representation_info
+mkdir -p ${savePath}
 
 layer=12
-saveFile=$savePath/explanation_words_representation_layer${layer}.csv
-explanation=../generate_explanation_files/explanation_CLS.txt
-python ${scriptDir}/match_representation.py --datasetFile $dataPath/layer$layer/${working_file}-layer${layer}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json --explanationFile $explanation --outputFile $saveFile
+saveFile=${savePath}/explanation_words_representation_layer${layer}.csv
+explanation=eraser_movie_dev/layer12/explanation/explanation_CLS.txt
+
+python ${scriptDir}/match_representation.py \
+    --datasetFile ${working_file}-layer${layer}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json \
+    --explanationFile ${explanation} \
+    --outputFile ${saveFile}
