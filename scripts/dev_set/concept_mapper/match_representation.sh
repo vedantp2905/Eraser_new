f47587796eee9c0614ad73d341b26f6b43bc454c
vedant29 <vedantpungliya29@gmail.com>
2025-05-05 00:35:38 -0500

    Updated run.sh

--- scripts/dev_set/concept_mapper/match_representation.sh
+++ scripts/dev_set/concept_mapper/match_representation.sh
@@ -16,7 +16,7 @@ savePath=eraser_movie_dev/layer${layer}/representation_info
 mkdir -p ${savePath}
 
 saveFile=${savePath}/explanation_words_representation_layer${layer}.csv
-explanation=eraser_movie_dev/layer${layer}/explanation/explanation_CLS.txt
+explanation=eraser_movie_dev/IG_explanation_files_mass_50/explanation_layer_6.txt
 
 python ${scriptDir}/match_representation.py \
     --datasetFile ${working_file}-layer${layer}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json \
